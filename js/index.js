4bb5279c9b9e5e1660408a4c0f4f4e1c7daa29e1
Carlos Calgaro <carlos.calgaro@keyworks.com.br>
2019-07-10 10:33:57 -0300

    Updates

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+
+$('.cd100').countdown100({
+    /*Set Endtime here*/
+    /*Endtime must be > current time*/
+    endtimeYear: 2020,
+    endtimeMonth: 02,
+    endtimeDate: 27,
+    endtimeHours: 13,
+    endtimeMinutes: 30,
+    endtimeSeconds: 0,
+    timeZone: "" 
+    // ex:  timeZone: "America/New_York"
+    //go to " http://momentjs.com/timezone/ " to get timezone
+});
+
+$("document").ready(function(){
+
+})
